18e5c8ec8488d22badeb97634d421885c5c3da8e
jjhc5 <jjhc572@gmail.com>
2022-03-30 00:52:27 +0900

    환경세팅 완료

---
+++ src/main/java/com/jdy/blog/JdyApplication.java
@@ -0,0 +1,13 @@
+package com.jdy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdy/blog/JdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
